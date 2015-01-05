0fa3e574858ef014e894d5c494ad6ac73b7c81c0
Jose Carrillo <82d.jumpmaster@gmail.com>
2015-01-05 11:13:17 -0500

    *

--- js/config.js
+++ js/config.js
@@ -10,10 +10,3 @@ $('a[href^="#"]').on('click', function(event) {
     }
 
 });
-
-
-if(window.navigator.standalone == true) {var overflow=function(el){el.addEventListener('touchstart',function(){var top=el.scrollTop,totalScroll=el.scrollHeight,currentScroll=top+el.offsetHeight
-if(top===0){el.scrollTop=1}else if(currentScroll===totalScroll){el.scrollTop=top-1}})
-el.addEventListener('touchmove',function(evt){if(el.offsetHeight<el.scrollHeight)
-evt._isScroller=true})}
-overflow(document.querySelector('#scroll'));document.body.addEventListener('touchmove',function(evt){if(!evt._isScroller){evt.preventDefault()}});}
